bdad9375e16bd710ac32eae2b826c72df98776f5
NathanTurpin <turpinn.all@gmail.com>
2024-10-02 11:25:55 +0200

    trned component + currency composable

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,47 @@
 <template>
-  <div>index page</div>
+  <div>
+    <section class="flex items-center justify-between mb-10">
+      <h1 class="text-4xl font-extrabold">Summary</h1>
+      <div>
+        <USelectMenu v-model="viewSelected" :options="transactionViewOptions" />
+      </div>
+    </section>
+    <section
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10"
+    >
+      <Trend
+        color="green"
+        title="Income"
+        :amount="4000"
+        :last-amount="3000"
+        :loading="false"
+      />
+      <Trend
+        color="red"
+        title="Income"
+        :amount="4000"
+        :last-amount="5000"
+        :loading="false"
+      />
+      <Trend
+        color="green"
+        title="Income"
+        :amount="4000"
+        :last-amount="3000"
+        :loading="false"
+      />
+      <Trend
+        color="red"
+        title="Income"
+        :amount="4000"
+        :last-amount="5000"
+        :loading="false"
+      />
+    </section>
+  </div>
 </template>
+
+<script setup >
+import { transactionViewOptions } from "../constants.js";
+const viewSelected = ref(transactionViewOptions[0]);
+</script>
